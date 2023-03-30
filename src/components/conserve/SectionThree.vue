aaa977299f8d6f3048c277069e5850c3bb70a4fa
macsakarn <macsakarn@gmail.com>
2023-03-31 00:41:11 +0700

    90% section 3&4

---
+++ src/components/conserve/SectionThree.vue
@@ -0,0 +1,57 @@
+<template>
+  <section
+    class="px-10 md:px-40 py-12 space-y-12 h-screen bg-bottom bg-contain lg:bg-[length:60vw]"
+  >
+    <h1 class="text-2xl font-bold text-primary-900">กิจกรรมในองค์กร</h1>
+  </section>
+
+  <section class="px-10 md:px-40 pt-12 mt-12 bg-primary-100">
+    <ol
+      class="border-l-8 border-primary-800 md:flex md:justify-center md:gap-6 md:border-l-0 md:border-t-8"
+    >
+      <li class="w-1/3">
+        <div class="flex items-center pt-2 md:block md:pt-0">
+          <div
+            class="-ml-6 -mt-5 mr-3 h-10 w-10 rounded-full bg-primary-800 md:ml-0 md:mr-0 md:-mt-6"
+          ></div>
+          <p class="mt-2 text-xl text-neutral-500">2563</p>
+        </div>
+        <div class="mt-2 ml-4 pb-5 md:ml-0">
+          <h4 class="mb-1.5 text-xl font-semibold"></h4>
+          <p class="mb-3 text-neutral-500"></p>
+        </div>
+      </li>
+      <li class="w-1/3">
+        <div class="flex items-center pt-2 md:block md:pt-0">
+          <div
+            class="-ml-6 -mt-5 mr-3 h-10 w-10 rounded-full bg-primary-800 md:ml-0 md:mr-0 md:-mt-6"
+          ></div>
+          <p class="mt-2 text-xl text-neutral-500">2571</p>
+        </div>
+        <div class="mt-2 ml-4 pb-5 md:ml-0">
+          <h4 class="mb-1.5 text-xl font-semibold">ผลิตและพัฒนาครู</h4>
+          <p class="mb-3 text-neutral-500"></p>
+        </div>
+      </li>
+      <li class="w-1/3">
+        <div class="flex items-center pt-2 md:block md:pt-0">
+          <div
+            class="-ml-6 -mt-5 mr-3 h-10 w-10 rounded-full bg-primary-800 md:ml-0 md:mr-0 md:-mt-6"
+          ></div>
+          <p class="mt-2 text-xl text-neutral-500">2576</p>
+        </div>
+        <div class="mt-2 ml-4 pb-5 md:ml-0">
+          <h4 class="mb-1.5 text-xl font-semibold">สนับสนุนและติดตาม</h4>
+          <p class="mb-3 text-neutral-500"></p>
+        </div>
+      </li>
+    </ol>
+  </section>
+</template>
+
+<style scoped>
+section {
+  background-image: url("/image/conserce/event-organization.png");
+  background-repeat: no-repeat;
+}
+</style>
--- src/views/ConserveView.vue
+++ src/views/ConserveView.vue
@@ -1,17 +1,21 @@
 <template>
-    <Layout>
-        โครงการครูรัก(ษ์)ถิ่น
-    </Layout>
-    <!-- TODO โครงการครูรัก(ษ์)ถิ่น -->
+  <Layout>
+    <SectionThree />
+    <SectionFour />
+  </Layout>
+  <!-- TODO โครงการครูรัก(ษ์)ถิ่น -->
 </template>
 <script>
 import { defineComponent } from "vue";
 
 import Layout from "@/layouts/Layout.vue";
-
+import SectionThree from "@/components/conserve/SectionThree.vue";
+import SectionFour from "@/components/conserve/SectionFour.vue";
 export default defineComponent({
-    components: {
-        Layout
-    }
-})
+  components: {
+    Layout,
+    SectionThree,
+    SectionFour,
+  },
+});
 </script>
