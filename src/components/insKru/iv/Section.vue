90fd6d24dd3a88b24aa1fa837b036b9d34713817
macsakarn <macsakarn@gmail.com>
2023-03-30 17:58:04 +0700

    done

---
+++ src/components/insKru/iv/Section.vue
@@ -0,0 +1,40 @@
+<script>
+import Ball from "./Ball.vue";
+export default {
+  components: { Ball },
+  data() {
+    return {
+      projectEvents: [
+        { year: 2018, message: "เพจ insKru และเว็บไซต์ inskru.com" },
+        { year: 2019, message: "กลุ่มครูปล่อยของ" },
+        { year: 2020, message: "Signature Workshop" },
+        { year: 2021, message: "Buddy Kru Development Tool / key Milestone" },
+        {
+          year: 2012,
+          message: "ครูปล่อยของ PLC Day และเปิดใช้งาน Buddy Kru เต็มรูปแบบ",
+        },
+      ],
+    };
+  },
+};
+</script>
+<template>
+  <section class="px-10 md:px-40 pt-8 space-y-12 bg-primary-100">
+    <h1 class="text-2xl font-bold text-primary-700">กิจกรรมของโครงการ</h1>
+
+    <div
+      class="grid grid-cols-1 lg:grid-cols-5 gap-4 lg:gap-0 place-items-center pb-16"
+    >
+      <template
+        v-for="(projectEvent, index) in projectEvents"
+        :key="projectEvent.year"
+      >
+        <Ball
+          :number="index + 1"
+          :year="projectEvent.year"
+          :message="projectEvent.message"
+        />
+      </template>
+    </div>
+  </section>
+</template>
--- src/views/insKruView.vue
+++ src/views/insKruView.vue
@@ -1,15 +1,21 @@
-<template>
-  <Layout> โครงการ insKru </Layout>
-  <!-- TODO: insKru -->
-</template>
 <script>
 import { defineComponent } from "vue";
 
 import Layout from "@/layouts/Layout.vue";
+import SectionIII from "@/components/insKru/iii/Section.vue";
+import SectionIV from "@/components/insKru/iv/Section.vue";
 
 export default defineComponent({
   components: {
     Layout,
+    SectionIII,
+    SectionIV,
   },
 });
 </script>
+<template>
+  <Layout>
+    <SectionIII />
+    <SectionIV />
+  </Layout>
+</template>
