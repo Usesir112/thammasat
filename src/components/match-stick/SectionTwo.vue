69709335e186e16b3fa84498b7258bdeca0ac266
Super_Octo_Potato_44 <53961723+Usesir112@users.noreply.github.com>
2023-03-30 18:17:11 +0700

    MatchStick Page

---
+++ src/components/match-stick/SectionTwo.vue
@@ -0,0 +1,60 @@
+<template>
+  <section>
+    <div class="bg-primary-100 mt-12 py-4">
+      <div class="mx-10 md:mx-40">
+        <!-- Topic -->
+        <div class="text-primary-900">
+          <p class="text-4xl font-bold py-5">{{ topic }}</p>
+        </div>
+        <!-- Topic -->
+
+        <!-- Items -->
+        <div
+          class="grid justify-between text-primary-900 font-bold text-center md:grid-cols-3"
+        >
+          <div
+            v-for="(item, index) in items"
+            :key="index"
+            class="grid grid-cols-2 place-items-center mt-12 md:grid-cols-1 md:mx-3"
+          >
+            <img
+              :src="`/image/match-stick/section-two/${item.src}`"
+              alt="item-one"
+              class="h-3/4"
+            />
+            <div class="md:mt-5">
+              <p class="md:h-24">
+                {{ item.descr }}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <!-- Items -->
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      topic: "เป้าหมายของโครงการ",
+      items: [
+        {
+          src: "one.png",
+          descr: "การสร้างกลุ่มคนรุ่นต่อไป",
+        },
+        {
+          src: "two.png",
+          descr: "ความรู้ภูมิปัญญาท้องถิ่น",
+        },
+        {
+          src: "three.png",
+          descr: "ทัศนคติต่อการเคารพความแตกต่างหลากหลาย",
+        },
+      ],
+    };
+  },
+};
+</script>
--- src/views/MatchStickView.vue
+++ src/views/MatchStickView.vue
@@ -1,15 +1,39 @@
 <template>
-  <Layout> โครงการไม้ขีดไฟ </Layout>
-  <!-- TODO โครงการไม้ขีดไฟ -->
+  <Layout>
+    <!-- @/components/match-stick/SectionOne.vue -->
+    <SectionOne />
+
+    <!-- @/components/match-stick/SectionTwo.vue -->
+    <SectionTwo />
+
+    <!-- @/components/match-stick/SectionFour.vue -->
+    <SectionFour />
+
+    <!-- @/components/match-stick/SectionFive.vue -->
+    <SectionFive />
+
+    <!-- @/components/match-stick/SectionSix.vue -->
+    <SectionSix />
+  </Layout>
 </template>
 <script>
 import { defineComponent } from "vue";
 
 import Layout from "@/layouts/Layout.vue";
+import SectionOne from "@/components/match-stick/SectionOne.vue";
+import SectionTwo from "@/components/match-stick/SectionTwo.vue";
+import SectionFour from "@/components/match-stick/SectionFour.vue";
+import SectionFive from "@/components/match-stick/SectionFive.vue";
+import SectionSix from "@/components/match-stick/SectionSix.vue";
 
 export default defineComponent({
   components: {
     Layout,
+    SectionOne,
+    SectionTwo,
+    SectionFour,
+    SectionFive,
+    SectionSix,
   },
 });
 </script>
