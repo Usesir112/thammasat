155cd95d5198d20ed8a37d8b08e364035133fb8c
Super_Octo_Potato_44 <53961723+Usesir112@users.noreply.github.com>
2023-04-11 20:20:19 +0700

    Add Home

---
+++ src/components/Home/SectionOne.vue
@@ -0,0 +1,59 @@
+<template>
+  <section>
+    <div class="grid md:grid-cols-2 mt-10 mx-10 md:mx-40">
+      <div>
+        <!-- Head -->
+        <div>
+          <p class="text-5xl text-primary font-bold mt-3">
+            <span class="text-primary-900">โครงการ</span>
+            <span class="md:text-4xl">{{ topic }}</span>
+          </p>
+        </div>
+        <!-- Head -->
+
+        <!-- Description -->
+        <div class="text-primary-900 mt-5 md:text-lg">
+          <p>{{ description }}</p>
+        </div>
+        <!-- Description -->
+
+        <!-- Statistics -->
+        <div class="grid grid-cols-2 mt-10 place-items-center">
+          <div v-for="item in stat" :key="item.name" class="mt-5">
+            <span
+              class="bg-primary-100 px-20 py-1 rounded-xl text-2xl font-bold text-center"
+              >{{ item.amount }}</span
+            >
+            <p class="text-center mt-2 font-bold">{{ item.title }}</p>
+          </div>
+        </div>
+        <!-- Statistics -->
+      </div>
+
+      <!-- Thai map -->
+      <div class="mt-10">
+        <Thailand />
+      </div>
+      <!-- Thai map -->
+    </div>
+  </section>
+</template>
+<script>
+import Thailand from "@/components/Map/Thailand.vue";
+export default {
+  components: { Thailand },
+  data() {
+    return {
+      topic: "จัดการความรู้และขับเคลื่อนระบบการศึกษาและการเรียนรู้",
+      description:
+        "คือโครงการที่จัดทำขึ้นเพื่อหาแนวทางการขับเคลื่อนเครือข่ายด้านการศึกษาและการเรียนรู้ด้วยองค์ความรู้ในหลากหลายมิติ รวมถึงการวิเคราะห์ปัจจัยแห่งความสำเร็จและปัจจัยแห่งความล้มเหลว",
+      stat: [
+        { name: "province", title: "จังหวัด", amount: "00" },
+        { name: "district", title: "อำเภอ", amount: "00" },
+        { name: "project", title: "โครงการ", amount: "00" },
+        { name: "member", title: "ผู้เข้าร่วม(คน)", amount: "00" },
+      ],
+    };
+  },
+};
+</script>
