d09977da6f4704aaf69fe26ef9a00949590a228c
Super_Octo_Potato_44 <53961723+Usesir112@users.noreply.github.com>
2023-05-26 20:50:41 +0700

    01 Project

---
+++ src/components/starfish/SectionTwo.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <div class="bg-primary-100 mt-12 py-4">
+      <div data-aos="fade-down" class="grid place-items-center md:grid-cols-2 mx-10 md:mx-40">
+        <!-- Text -->
+        <div class="grid place-items-center font-semibold">
+          <p class="md:text-2xl">
+            โรงเรียนบ้านปลาดาวจัดตั้งขึ้นเมื่อวันที่ 1 พฤษภาคม พ.ศ. 2549
+            โดยมูลนิธิโรงเรียนสตาร์ฟิชคันทรีโฮม [Starfish Country Home School
+            Foundation (of Thailand)] เป็นผู้รับใบอนุญาต
+            เริ่มแรกได้อนุญาตเป็นโรงเรียนประเภทการศึกษาสงเคราะห์มาตรา 15 (3)
+            ระดับอนุบาล (ปฐมวัย) ชื่อโรงเรียนอนุบาลบ้านปลาดาว
+          </p>
+        </div>
+        <!-- Text -->
+
+        <!-- Image -->
+        <div>
+          <img
+            class="md:w-[80%]"
+            src="/image/01LongTermProject/01Starfish/logo.png"
+            alt="middle"
+          />
+        </div>
+        <!-- Image -->
+      </div>
+    </div>
+  </section>
+</template>
+<script setup>
+import { onMounted } from "vue";
+import AOS from "aos";
+
+onMounted(() => {
+    AOS.init();
+})
+
+</script>
